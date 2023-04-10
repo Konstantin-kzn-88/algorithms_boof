a9e6551887745a369d72880bcbbb589535b5b14e
Konstantin <kuznetsovkm@yandex.ru>
2023-04-10 13:58:26 +0300

    Manhattan distance

--- algorithms/2_Manhattan_distance.py
+++ algorithms/2_Manhattan_distance.py
@@ -1,10 +1,10 @@
 start_point = (int(input('x_start = ')), int(input('y_start = ')))
 end_point = (int(input('x_end = ')), int(input('y_end = ')))
 
-dx = abs(start_point[0]-end_point[0])
-dy = abs(start_point[1]-end_point[1])
+dx = abs(start_point[0] - end_point[0])
+dy = abs(start_point[1] - end_point[1])
 
-print(dx+dy)
+print(dx + dy)
 
 if __name__ == '__main__':
     pass
