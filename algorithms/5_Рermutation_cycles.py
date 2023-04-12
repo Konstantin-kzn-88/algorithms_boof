da50dde3dcc23993a436aaf8f1bbcf2510793235
Konstantin <kuznetsovkm@yandex.ru>
2023-04-12 13:06:48 +0300

    Циклы перестановки

---
+++ algorithms/5_Рermutation_cycles.py
@@ -0,0 +1,24 @@
+perm = [i - 1 for i in map(int, input().split())]
+
+visited = [False] * len(perm)
+
+ans = 0
+
+for start in range(len(perm)):
+    if visited[start]:
+        continue
+    ans += 1
+    current = start
+    print(f'current_for = {current}')
+    while True:
+        visited[current] = True
+        current = perm[current]
+        print(f'current_while = {current}')
+        if current == start:
+            print('______')
+            break
+
+print(ans)
+
+if __name__ == '__main__':
+    pass
